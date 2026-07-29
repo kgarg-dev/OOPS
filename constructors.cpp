112aa15bf53d407cf98b84cae2a59f27f10e046e
KRIZNA GARG <krishna garg222008@gmail.com>
2026-07-29 23:28:36 +0530

    error solution

--- constructors.cpp
+++ constructors.cpp
@@ -5,7 +5,9 @@ public:
     string name; 
     int roll;
     float gpa;
+    Student(){ // default constructor , when we create constructor system removes this itself so we need to redefine it to declare by simple method.
 
+    }
     Student(string s, int r, float g){ //constructors
         name=s;
         roll=r;
@@ -19,6 +21,12 @@ int main(){
     Student S1("Krishna Garg", 22 ,8.89); //writing value of arguments
     Student S2("Raghav Garg", 23 , 9.08);
 
+    Student S3; //declaring by simple method
+    S3.name="Arnav Bansal";
+    S3.roll=29;
+    S3.gpa=6.69;
+
     print(S1);
     print(S2);
+    print(S3)
 }
